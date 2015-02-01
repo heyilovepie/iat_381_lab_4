265f0996b50febb43791e4ddbbc001e5868aaaf2
heyilovepie <gillianramsay@shaw.ca>
2015-02-01 13:36:44 -0800

    Adding stuff

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,21 @@ someApp.config(function ($routeProvider) {
       templateUrl: 'pages/home.html',
       controller : 'mainController'
     })
+
+    .when('/about', {
+      templateUrl: 'pages/about.html',
+      controller : 'aboutController'
+    })
+
     .when('/clarity', {
       templateUrl: 'pages/clarity.html',
       controller : 'clarityController'
     })
 });
+
+window.onload = function(){
+	var mc = new Hammer(document.getElementById('hammer-pad'));
+	mc.on('pan', function(){
+		alert('You can\'t touch this');
+	});
+};
